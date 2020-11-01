1ab51a8e22ae8a3ac17e6ba9db51c8c0ede19337
David Rodriguez <darodrig@c3r10s1.42madrid.com>
2020-11-01 10:52:15 +0100

    dslr

---
+++ describe.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python3
+
+from sys import argv as av
+import pandas as pd
+import numpy as np
+
+if __name__ == "__main__":
+    if (len(av) < 2):
+        print("USAGE: ./describe.py [dataset].csv")
+        exit(1)
+    df = pd.read_csv(av[1])
+    print(df.__sizeof__)
